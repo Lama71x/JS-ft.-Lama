1efffe55125eb41f3023e1b6b81a5fb9d8a5816f
Lama71x <webdev9849@gmail.com>
2025-04-18 09:43:16 +0000

    Conversio of Number

---
+++ 01-Basic/03-conversion.js
@@ -0,0 +1,17 @@
+ // The conversion done below is number to string And it is true
+// let score = 10
+// console.log(score)
+// console.log(typeof score)
+
+// let stringScore = String(score)
+// console.log(typeof stringScore);
+
+
+//converting name to number but value is NaN(Not a Number)
+// let myName = "Lama"
+// console.log(typeof myName);
+
+// let nameToNumber = Number(myName)
+// console.log(typeof nameToNumber);
+
+
