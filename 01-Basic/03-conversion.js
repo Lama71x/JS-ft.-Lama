327a180a90eb989273a6e4ab73f4634c4cc69525
Lama71x <webdev9849@gmail.com>
2025-04-19 03:46:59 +0000

    conversion and Comparison of datatype

--- 01-Basic/03-conversion.js
+++ 01-Basic/03-conversion.js
@@ -14,4 +14,47 @@
 // let nameToNumber = Number(myName)
 // console.log(typeof nameToNumber);
 
+// ********** operation **********
 
+// let value = 3
+// let negValue = -value;
+// console.log(negValue)
+
+//  console.log(2+2);   //Add
+//  console.log(2-2);  //Subtraction
+//  console.log(2*2);   //Multiply
+//  console.log(2**3);  //power
+//  console.log(2/3);  //Division
+//  console.log(2%3);  //Remainder
+ 
+ 
+ //In String 
+//  let str1 = "I Love "
+// let str2 = "Lama"
+// let str3 = str1 + str2;
+// console.log(str3)
+
+//In Number --Confusing term
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+//In conclusion : It abstract from first data type
+
+
+//In boolean
+//true = 1
+//false = 0
+
+// console.log(true)
+// console.log(+true)
+// console.log(true+true);
+// console.log(-true)
+// console.log(+false)
+// console.log(false+false);
+
+//increament
+
+let gameCounter = 100;
+gameCounter ++;
+console.log(gameCounter);
