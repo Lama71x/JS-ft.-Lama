5186b540ce6b32176868d62b16d11488354ef524
Lama71x <webdev9849@gmail.com>
2025-04-20 13:14:50 +0000

    The Fortune Taller

---
+++ Assignment/V1_Fortune.js
@@ -0,0 +1,11 @@
+// Why pay a fortune teller when you can just program your fortune yourself?
+
+// Store the following into variables: number of children, partner's name, geographic location, job title.
+// Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+
+let numChild = 3
+let partnerName = "Bulbul"
+let geoLocation = "Kathmandu"
+let jobTitle = "Web Developer"
+
+console.log("You will be a " + jobTitle + " in " + geoLocation + ", and Married to " + partnerName + "with" + numChild + " kids");
