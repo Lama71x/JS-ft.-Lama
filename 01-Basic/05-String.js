90e6cc0274e2e3a66b13719558bf4f7a49497417
Lama71x <webdev9849@gmail.com>
2025-04-21 03:21:03 +0000

    Sring in JavaScript

---
+++ 01-Basic/05-String.js
@@ -0,0 +1,36 @@
+// let myYoutube = "SarojLama1"
+// let anotherName = myYoutube
+
+// anotherName = "Bulbul"
+
+// console.log("Before My Id name was " + myYoutube + "And its now "  + anotherName);
+
+
+
+// let userOne = {
+//     email: "user@gmail.com" ,
+//     esewa: "user77"
+// };
+
+// let userTwo = userOne;
+// userTwo.email = "lama@gmail.com"
+
+// console.log(userOne.email);
+
+
+
+// const name = "Saroj"
+// const repoCount = 50
+
+// console.log(`Hello My name is ${name} and My repo count is ${repoCount}`);
+
+
+// const gameName = new String("Saroj")
+// console.log(gameName[4]);
+
+
+
+
+
+
+
