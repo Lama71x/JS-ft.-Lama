f2caf42a225185c41501913552728e7702bee22b
Lama71x <webdev9849@gmail.com>
2025-04-24 15:38:19 +0000

    Date and Time

---
+++ 01-Basic/07_datasinJs.js
@@ -0,0 +1,11 @@
+// let myDate = new Date()
+// console.log(myDate)
+
+// console.log(myDate.toString()); //To get in Normal Formate
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+
+
+// let myCreatedDate = new Date(2025, 6, 25)
+let myCreatedDate = new Date("06-25-2025")
+console.log(myCreatedDate)
