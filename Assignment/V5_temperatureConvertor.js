89af09f140d4fa9d20db99f1c0a90be5d4629bf4
Lama71x <webdev9849@gmail.com>
2025-04-26 02:15:28 +0000

    Convert temperature

---
+++ Assignment/V5_temperatureConvertor.js
@@ -0,0 +1,13 @@
+// The Temperature Converter
+// It's hot out! Let's make a converter based on the steps here.
+
+// Store a celsius temperature into a variable.
+// Convert it to fahrenheit and output "NN°C is NN°F".
+// Now store a fahrenheit temperature into a variable.
+// Convert it to celsius and output "NN°F is NN°C."
+
+let celsius = 122
+let calculate = (celsius * 9/5) + 32;
+let fahrenheit = calculate 
+
+console.log(celsius + ".F is" + fahrenheit + " .C." )
