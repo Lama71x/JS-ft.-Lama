6cd6f4ba5f860a6c1039142ceb66027cbb36fe7f
Lama71x <webdev9849@gmail.com>
2025-04-26 02:06:20 +0000

    The Geometrizer in Js

---
+++ Assignment/V4_geometrizer.js
@@ -0,0 +1,16 @@
+// The Geometrizer
+// Calculate properties of a circle, using the definitions here.
+
+// Store a radius into a variable.
+// Calculate the circumference based on the radius, and output "The circumference is NN".
+// Calculate the area based on the radius, and output "The area is NN".
+
+let radius = 2.5
+let circumference = Math.PI *2*radius
+
+console.log("The circumference is " + circumference );
+
+let area = Math.PI *radius*radius
+
+console.log("The area is "+area);
+
