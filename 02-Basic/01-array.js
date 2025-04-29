19a15b2f15c10a155e7bac322593573623d9ee9f
Lama71x <webdev9849@gmail.com>
2025-04-29 15:14:52 +0000

    Array

--- 02-Basic/01-array.js
+++ 02-Basic/01-array.js
@@ -1,1 +1,28 @@
-const myArr = [0, 1, 2, 3, 4 , 5]
+// const myArr = [0, 1, 2, 3, 4 , 5]
+// // console.log(myArr[0]); Identify the place
+
+// //Methods of Array
+
+// // console.log(myArr.push(6));
+// // // console.log(myArr.pop(5));
+// //  console.log(myArr.shift(5));
+// console.log("A",myArr);
+
+//slice
+
+//slice
+
+// const myn1 = myArr.slice(1,4)
+// console.log(myn1);
+
+// *******************Part 2***************************
+
+// const marvel_heros = ["Thor" , "IronMan" , "Spiderman"]
+
+// const dc_heros = ["Superman" , "flash" , "Batman"]
+
+// console.log(marvel_heros.push(dc_heros))
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+
