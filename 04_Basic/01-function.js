99ea0891976f04df69b3c15f0fc8b8248528abbe
Lama71x <webdev9849@gmail.com>
2025-05-07 10:54:15 +0000

    Function

---
+++ 04_Basic/01-function.js
@@ -0,0 +1,26 @@
+//How we call normally 
+// console.log("S");
+// console.log("A");
+// console.log("R");
+// console.log("O");
+// console.log("J");
+
+
+//In function
+
+// function myName(){
+//     console.log("S");
+//     console.log("A");
+//     console.log("R");
+//     console.log("O");
+//     console.log("J");
+// }
+// myName()
+
+
+
+function addTwoNumber(num1 , num2){
+    console.log(num1+num2);
+    
+}
+addTwoNumber(2,6)
