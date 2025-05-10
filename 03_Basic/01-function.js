a460ef4de8a5637cea2278fbc068ec9477020cf9
Lama71x <webdev9849@gmail.com>
2025-05-10 17:04:08 +0000

    Function part 2

---
+++ 03_Basic/01-function.js
@@ -0,0 +1,78 @@
+//How we call normally 
+// console.log("S");
+// console.log("A");
+// console.log("R");
+// console.log("O");
+// console.log("J");
+
+
+//In function
+
+// function myName(){
+//     console.log("S");
+//     console.log("A");
+//     console.log("R");
+//     console.log("O");
+//     console.log("J");
+// }
+// myName()
+
+
+
+// function addTwoNumber(num1 , num2){
+//     console.log(num1+num2);
+// }
+// addTwoNumber(2,6)
+
+// function addTwoNumber(num1 , num2){
+//     // let result = num1 + num2
+//     // return result    //this is take 1
+// }
+// const result = addTwoNumber(2,6)
+
+// console.log(result);
+
+// function addTwoNumber(num1 , num2){
+    //     console.log(num1+num2);
+    // }
+
+// function calculateCartPrice(num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(9))
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200,300,500,600))
+
+
+//Object in Function
+
+// const user = {
+//     username : "Lama",
+//     price : 355
+// }
+
+// function handleObject(anyObject){
+//     console.log(`Username of ${anyObject.username} and its price is ${anyObject.price}`);
+// }
+
+// handleObject(user)
+
+//Direct object
+// handleObject({
+//     username: "Lama",
+//     price : 555
+// })
+
+const myNewArray = [200,300,500,6000]
+
+function returnSecondaryValue(getArray){
+    return getArray
+}
+
+console.log(returnSecondaryValue(myNewArray[0]))
+
