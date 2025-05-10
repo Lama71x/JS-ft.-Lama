9f700cca718101fe9ac638bf19be6e4563eb1687
Lama71x <webdev9849@gmail.com>
2025-05-10 17:03:52 +0000

    Global and Local Scope

---
+++ 03_Basic/02_scope.js
@@ -0,0 +1,24 @@
+//Scopes
+// const a = 10
+// let b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);  ..This is simple and we all know
+
+// let b = 3000    ------->This is a global Scope
+// if(true) {
+//     const a = 10
+//     let b = 20           ----->This is Local Scope
+//     var c = 30
+// }
+
+// console.log(a);  //This will not print
+// console.log(b);     //This will also not print
+// console.log(c);  this will print so not to use var this may cause problem
+
+
+
+
+
