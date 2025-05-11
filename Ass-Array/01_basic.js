542b659c726e253d143a6c1843bdb07cf40531b1
Lama71x <webdev9849@gmail.com>
2025-05-11 18:24:52 +0000

    Basic task

---
+++ Ass-Array/01_basic.js
@@ -0,0 +1,37 @@
+// 01--Create an array named colors with 5 different color names. Print the first and last color.
+// Solution
+
+//  const colorsName = ["Red", "Blue", "Yellow", "Black", "Pink"]
+
+
+// console.log("This is the First color: ",colorsName.at(0));
+// console.log("This is the Last color: ", colorsName[colorsName.length-1]);
+
+
+// 02--	Add the value "Purple" to the end of an array using JavaScript.
+// console.log(colorsName , colorsName.push("Purple"))
+
+
+// 3.	Remove the first element of an array and print the updated array.
+
+// colorsName.shift()
+// console.log("Updated Data: " + colorsName )
+
+
+// 4.	Given an array of numbers, print only the even numbers.
+
+// const givenNumber = [1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99,100]
+// let result = givenNumber.filter(num => num%2 === 0)
+// console.log("Even no. : ", result);
+
+
+// // only Odd Number
+
+// let Odd = {
+//     result : givenNumber.filter(num => num % 2 !== 0)
+// }
+
+// console.log(Odd);
+
+
+// 5.	Write a program to count how many elements are in an array.
