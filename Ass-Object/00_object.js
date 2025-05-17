05d9fb31399c69a7d96bbc1f2f44efda23fdbbe4
Lama71x <webdev9849@gmail.com>
2025-05-17 16:08:25 +0000

    Object

---
+++ Ass-Object/00_object.js
@@ -0,0 +1,36 @@
+// // //  1. Create a Simple Object
+// // // Q: Create an object called student with the properties: name, age, and isEnrolled.
+
+// // const student = {
+// //     name:"Lama",
+// //     age:21,
+// //     isEnrolled: true
+// // }
+
+// // console.log(`My name is ${student.name} my age is ${student.age} and i am always isEnrolled in my class.`);
+
+
+// //  2. Access Object Property
+// // Q: Given the object below, how do you access the city value?
+
+// // const address = {
+// //   street: "Main St",
+// //   city: "Kathmandu",
+// //   zip: "44600"
+// // };
+
+// // console.log(address.city);
+
+
+//  4. Delete a Property
+// Q: Delete the age property from the following object:
+
+const person = {
+  name: "Saroj",
+  age: 22,
+  hobby: "Coding"
+};
+
+console.log(person.delete(age))
+console.log(person)
+
