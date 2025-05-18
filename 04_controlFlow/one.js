908c0b78c7c4c5eddbbf1e19b97d16838a31767e
Lama71x <webdev9849@gmail.com>
2025-05-18 02:37:55 +0000

    control Flow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,48 @@
+// //condition statement
+// const userIdLoggedin = false
+
+
+// if(userIdLoggedin == true){
+//     console.log("Hello Welcome")
+// } else {
+//     console.log("Please Sign up!!")
+// }
+
+//== and === different between them
+
+// if(2=="2"){
+//     console.log("true")
+// }
+
+// if(2==="2"){
+//     console.log("not true");
+    
+// }
+
+//nasted condition
+
+// const mark = 55
+
+// if(mark < 25){
+//     console.log("NG");
+// } else if(mark < 40){
+//     console.log("You got C");
+// } else if(mark < 50){
+//     console.log("You got c+");
+    
+// }else if(mark < 60){
+//     console.log("You got B");
+    
+// }else if(mark < 70){
+//     console.log("You got B+");
+    
+// }else if(mark < 80){
+//     console.log("You got A");
+    
+// }else if(mark < 90){
+//     console.log("You got A+");
+    
+// } else if(mark <= 100){
+//     console.log("You got A+");
+    
+// }
