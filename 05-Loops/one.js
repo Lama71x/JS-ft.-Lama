42424fcd25324d96fbea39f924d993ff024898f9
Lama71x <webdev9849@gmail.com>
2025-06-03 15:32:21 +0000

    #

--- 05-Loops/one.js
+++ 05-Loops/one.js
@@ -6,3 +6,22 @@
 //     const element = i;
 //     console.log(element);
 // }
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+
+    console.log(`Demo of 1 ${i}`);
+
+    
+
+    for (let j = 10; j < 20; j++) {
+        const element = j;
+
+        console.log(`Demo part 2 ${j}`)
+        
+    }
+    
+    // console.log(`here is ${i} and this is ${j}`);
+    
+
+}
