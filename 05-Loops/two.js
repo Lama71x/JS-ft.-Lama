bfd82bfffe06bbd21ce40df302a2471df1860605
Lama71x <webdev9849@gmail.com>
2025-06-05 02:22:58 +0000

    While and do while loop

---
+++ 05-Loops/two.js
@@ -0,0 +1,28 @@
+// //This is a part two on loops which is While loop and Do While loop
+
+
+// // let i = 0
+// // while (i <= 10) {
+// //     console.log(`My name is ${i}`);
+// //     i = i++
+// // }
+
+// //This is showing continus Result
+
+// let hero = ["Saktiman" , "Spiderman" , "Batman"]
+
+// let myArr = 0
+
+// while (myArr = hero.length) {
+
+//     console.log(`Value is ${hero[myArr]}`)
+
+//     myArr = myArr++
+// }
+
+let score = 1
+
+do{
+    console.log(`Score is ${score}`)
+    score++
+}while(score <= 10);  //Successful
